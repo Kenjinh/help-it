3137cdc193faea7eb37a3f2501d3a0955873757c
Gabriel Kenji dos Santos Nohara <70982015+Kenjinh@users.noreply.github.com>
2022-11-28 17:19:57 -0300

    .

--- bot/curso/views.py
+++ bot/curso/views.py
@@ -20,15 +20,19 @@ class ChatterBotApiView(View):
     chatterbot = ChatBot('Help-It', logic_adapters=['chatterbot.logic.BestMatch', 'chatterbot.logic.MathematicalEvaluation'],storage_adapter='chatterbot.storage.SQLStorageAdapter',
     database_uri='sqlite:///database.sqlite3'
     )
-    trainer = ChatterBotCorpusTrainer(chatterbot)
-    trainer.train('chatterbot.corpus.portuguese')
-    trainer = ListTrainer(chatterbot)
-    trainer.train([
-        'Oi',
-        'Olá sou o Help-it, como posso te ajudar?',
-        'O que você faz?',
-        'Sou um chatbot, posso te ajudar com dúvidas sobre a área de tecnologia',
-    ])
+    # trainer = ChatterBotCorpusTrainer(chatterbot)
+    # trainer.train('chatterbot.corpus.portuguese')
+    # trainer = ListTrainer(chatterbot)
+    # trainer.train([
+    #     'Oi',
+    #     'Olá sou o Help-it, como posso te ajudar?',
+    #     'O que você faz?',
+    #     'Sou um chatbot, posso te ajudar com dúvidas sobre a área de tecnologia',
+    #     'Qual a sua função?',
+    #     'Sou um chatbot, posso te ajudar com dúvidas sobre a área de tecnologia',
+    #     'Qual o seu nome?',
+    #     'Meu nome é Help-it',
+    # ])
 
 
 
@@ -49,31 +53,43 @@ class ChatterBotApiView(View):
 
         response = self.chatterbot.get_response(input_data)
         response_data = response.serialize()
-        # response_data['in_response_to'] = response_data['in_response_to'].lower().replace('-','')  # Remove "-" and change for lower
-        # response_data['in_response_to'] = unidecode(response_data['in_response_to']) # Remove accents
-        # # region Welcome
-        # if response_data['in_response_to'] == 'ola' or response_data['in_response_to'] == 'oi' or response_data[
-        #     'in_response_to'] == 'olá' or response_data['in_response_to'] == 'começar':
-        #     response_data[
-        #         'text'] = 'Seja bem-vindo, sou o Help-it e estarei te auxiliando a escolher algum determinado curso ou auxiliando a aprender sobre algumas das áreas de TI!Escolha uma das seguintes opções:' + \
-        #                   '</br><button id="buttonSay" class="btn btn-outline-dark mt-2" onClick="sendButton(this.value);" value="Quero visualizar os cursos disponíveis">Quero visualizar os cursos disponíveis</button>'
+        response_data['in_response_to'] = response_data['in_response_to'].lower().replace('-','')  # Remove "-" and change for lower
+        response_data['in_response_to'] = unidecode(response_data['in_response_to']) # Remove accents
+        # region Welcome
+        if response_data['in_response_to'] == 'ola' or response_data['in_response_to'] == 'oi' or response_data[
+             'in_response_to'] == 'olá' or response_data['in_response_to'] == 'começar':
+             response_data[
+                 'text'] = 'Bem-vindo ao help IT chatbot espero que consigamos te auxiliar no seu problema. Clique em qual ferramenta está sendo problema:' + \
+                           '</br><button id="buttonSay" class="btn btn-outline-dark mt-2" onClick="sendButton(this.value);" value="Outlook">Outlook</button>' + \
+                           '</br><button id="buttonSay" class="btn btn-outline-dark mt-2" onClick="sendButton(this.value);" value="Cisco Jabber">Cisco Jabber</button>' + \
+                           '</br><button id="buttonSay" class="btn btn-outline-dark mt-2" onClick="sendButton(this.value);" value="Citrix">Citrix</button>'
 
-        # elif response_data['in_response_to'] == 'desejo entender mais sobre as areas de ti':
-        #     response_data['text'] = '????'
-        # # endregion
+        elif response_data['in_response_to'] == 'desejo entender mais sobre as areas de ti':
+             response_data['text'] = '????'
+        # endregion
 
-        # # region Select Train
-        # elif response_data['in_response_to'] == 'quero visualizar os cursos disponiveis':
-        #     response_data['text'] = 'Em qual área você tem interesse em treinamentos:' + \
-        #                             '</br><button id="buttonSay" class="btn btn-outline-dark mt-2" onClick="var buttonValue = sendButton(this.value);" value="Desenvolvimento Front-end">Desenvolvimento Front-end</button>' + \
-        #                             '</br><button id="buttonSay" class="btn btn-outline-dark mt-2" onClick="var buttonValue = sendButton(this.value);" value="Desenvolvimento Back-end">Desenvolvimento Back-end</button>' + \
-        #                             '</br><button id="buttonSay" class="btn btn-outline-dark mt-2" onClick="var buttonValue = sendButton(this.value);" value="Mobile">Desenvolvimento Mobile</button>' + \
-        #                             '</br><button id="buttonSay" class="btn btn-outline-dark mt-2" onClick="var buttonValue = sendButton(this.value);" value="git e github">Versionamento de códigos com Git e GitHub</button>' + \
-        #                             '</br><button id="buttonSay" class="btn btn-outline-dark mt-2" onClick="var buttonValue = sendButton(this.value);" value="Redes de computadores">Redes de computadores</button>' + \
-        #                             '</br><button id="buttonSay" class="btn btn-outline-dark mt-2" onClick="var buttonValue = sendButton(this.value);" value="automação">Criação de automação de testes</button>' + \
-        #                             '</br><button id="buttonSay" class="btn btn-outline-dark mt-2" onClick="var buttonValue = sendButton(this.value);" value="Banco de dados">Banco de dados</button>' + \
-        #                             '</br><button id="buttonSay" class="btn btn-outline-dark mt-2" onClick="var buttonValue = sendButton(this.value);" value="ola">Voltar ao menu inicial</button>'
-        # # endregion
+        # region outlook
+        elif response_data['in_response_to'] == 'outlook':
+             response_data['text'] = 'Em qual área você precisa de ajuda:</br>' + \
+                                     'Outlook – Siga as seguintes instruções</br>' + \
+                                     '1.	Cheque se está conectado com a internet</br>'+ \
+                                     '2.	Reinicie o computador</br>'+ \
+                                     '3.	Passando atendimento para analista de 2° nível</br>'
+        # endregion
+        #region Cisco Jabber
+        elif response_data['in_response_to'] == 'cisco jabber':
+             response_data['text'] = 'Cisco Jabber - Siga as seguintes instruções:</br>' + \
+                                     '1.	Cheque se está conectado com a internet</br>'+ \
+                                     '2.	Reinicie o computador</br>'+ \
+                                     '3.	Passando atendimento para analista de 2° nível</br>'
+        # endregion
+        #region Citrix
+        elif response_data['in_response_to'] == 'citrix':
+                response_data['text'] = 'Citrix - Siga as seguintes instruções:</br>' + \
+                    '1.	Cheque se está conectado com a internet</br>'+\
+                    '2.	Reinicie o computador</br>'+\
+                    '3.	Passando atendimento para analista de 2° nível</br>'
+        # endregion
         return JsonResponse(response_data, status=200)
 
     @csrf_exempt
@@ -88,6 +104,6 @@ class ChatterBotApiView(View):
 def home(request):
     context = {
         'year': datetime.now().year,
-        'title': 'Home Page',
+        'title': 'Help IT - Home Page',
     }
     return render(request, "html/index.html", context)
--- bot/static/js/buttons.js
+++ bot/static/js/buttons.js
@@ -1,5 +1,5 @@
-createRowBot('Seja bem-vindo, sou o Curso em bot e estarei te auxiliando a escolher algum determinado curso ou auxiliando a aprender sobre algumas das áreas de TI!Escolha uma das seguintes opções:' +
-    '</br><button id="buttonSay" class="btn btn-outline-dark mt-2" onClick="sendButton(this.value);" value="Quero visualizar os cursos disponíveis">Quero visualizar os cursos disponíveis</button>');
+createRowBot('Bem-vindo ao help IT chatbot espero que consigamos te auxiliar no seu problema. Clique em qual ferramenta está sendo problema:' +
+    '</br><button id="buttonSay" class="btn btn-outline-dark mt-2" onClick="sendButton(this.value);" value="Outlook">Outlook</button></br><button id="buttonSay" class="btn btn-outline-dark mt-2" onClick="sendButton(this.value);" value="Cisco Jabber">Cisco Jabber</button></br><button id="buttonSay" class="btn btn-outline-dark mt-2" onClick="sendButton(this.value);" value="Citrix">Citrix</button>');
 
 
 
